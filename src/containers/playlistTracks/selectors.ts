36ce170c20da918a6e13acae37614ee35d54741e
Leandro Silva de Carvalho <82327995+LeandroSilvaCarvalho@users.noreply.github.com>
2025-05-15 12:28:50 +0200

    change on create playlist

--- src/containers/playlistTracks/selectors.ts
+++ src/containers/playlistTracks/selectors.ts
@@ -5,7 +5,3 @@ const selectSelf = (state: RootState) => state.playlistTracks;
 
 export const selectPlaylistTracks = createSelector(selectSelf, (state) => state.items);
 export const selectPlaylistTracksStatus = createSelector(selectSelf, (state) => state.status);
-export const selectPlaylistTracksAddingStatus = createSelector(
-  selectSelf,
-  (state) => state.addingStatus
-);
